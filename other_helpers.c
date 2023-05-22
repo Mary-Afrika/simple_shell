8a5bae744b740feb794f23b294cc982bda219a71
Mary-Afrika <marygahar@gmail.com>
2023-05-22 13:32:32 -0700

    trailing whitespaces

--- other_helpers.c
+++ other_helpers.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- * _isdigit - checks whether a given input is a digit
- * @term: a char int parameter
- * Return: 1 if c is a digit, 0 otherwise
- */
+* _isdigit - checks whether a given input is a digit
+* @term: a char int parameter
+* Return: 1 if c is a digit, 0 otherwise
+*/
 
 int _isdigit(int term)
 {
-	if (term >= 48 && term <= 57)
-	{
-		return (1);
-	}
-	else
-		return (0);
+if (term >= 48 && term <= 57)
+{
+return (1);
+}
+else
+return (0);
 }
