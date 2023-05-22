59c164321b1560b0aa7cfe76985dcf8535d7f6f2
Mary-Afrika <marygahar@gmail.com>
2023-05-22 12:47:53 -0700

    added args

--- handle_customs.c
+++ handle_customs.c
@@ -1,95 +1,99 @@
 #include "shell.h"
 
 /**
- * handle_custom_commands - handles custom commands
- * beyond the built in commands
- * @command: the cammand arg , args[0]
- * Return: 1 if found else -1
+* handle_custom_commands - handles custom commands
+* beyond the built in commands
+* @command: the cammand arg , args[0]
+* @args: arguments passed
+* Return: 1 if found else -1
 */
 int handle_custom_commands(char *command, char **args)
 {
-	(void)args;
-	if (_strcmp(command, "exit") == 0)
-	{
-		my_exit(args);
-		return (1);
-	}
-	else if (_strcmp(command, "setenv") == 0)
-	{
-		if (args[1] && args [2])
-		{
-			_setenv(args[1], args[2], 1);
-		} else {
-			printf("Setenv failed. Invalid arguments.\n");
-		}
-		return (1);
-	}
-	else if (_strcmp(command, "unsetenv") == 0)
-	{
-		if (args[1])
-		{
-			_unsetenv(args[1]);
-		} else {
-			printf("Unsetenv failed. Invalid arguments.\n");
-		}
-		return (1);
-	}
-
-	else if (_strcmp(command, "cd") == 0)
-	{
-		change_directory(args);
-		return (1);
-	}
+(void)args;
+if (_strcmp(command, "exit") == 0)
+{
+my_exit(args);
+return (1);
+}
+else if (_strcmp(command, "setenv") == 0)
+{
+if (args[1] && args[2])
+{
+_setenv(args[1], args[2], 1);
+}
 else
-	{
-		return (-1);
-	}
+{
+perror("Setenv failed. Invalid arguments.\n");
+}
+return (1);
+}
+else if (_strcmp(command, "unsetenv") == 0)
+{
+if args[1]
+{
+_unsetenv(args[1]);
+}
+else
+{
+perror("Unsetenv failed. Invalid arguments.\n");
+}
+return (1);
+}
+
+else if (_strcmp(command, "cd") == 0)
+{
+change_directory(args);
+return (1);
+}
+else
+{
+return (-1);
+}
 }
 
 /**
- * change_directory - changes directory suitably
- * @args: string array of input promt
- * Return: void
+* change_directory - changes directory suitably
+* @args: string array of input promt
+* Return: void
 */
+
 void change_directory(char **args)
 {
-	char cwd[256];
-	char *home_dir;
-	char *old_dir;
+char cwd[256];
+char *home_dir;
+char *old_dir;
 
-	home_dir = getenv("HOME");
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		printf("getcwd error");
-		return;
-	}
-
-	if (args[1] == NULL)
-	{
-		chdir(home_dir);
-	}
-	else
-	{
-		if (_strcmp(args[1], "-") == 0)
-		{
-			old_dir = getenv("OLDPWD");
-			if (old_dir != NULL)
-			{
-				chdir(old_dir);
-				printf("%s\n", old_dir);
-			}
-			else
-			{
-				printf("cd: OLDPWD not set\n");
-			}
-		}
-		else
-		{
-			if (chdir(args[1]) == -1)
-			{
-				printf("cd: %s: No such file or directory\n", args[1]);
-			}
-		}
-	}
+home_dir = getenv("HOME");
+if (getcwd(cwd, sizeof(cwd)) == NULL)
+{
+perror("getcwd error");
+return;
 }
-
+if (args[1] == NULL)
+{
+chdir(home_dir);
+}
+else
+{
+if (_strcmp(args[1], "-") == 0)
+{
+old_dir = getenv("OLDPWD");
+if (old_dir != NULL)
+{
+chdir(old_dir);
+perror("%s\n", old_dir);
+}
+else
+{
+perror("cd: OLDPWD not set\n");
+}
+}
+else
+{
+if (chdir(args[1]) == -1)
+{
+perror("cd: %s: No such file or directory\n", args[1]);
+}
+}
+}
+}
