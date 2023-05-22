e3f4cadb820427c8db8f25c8d879e7583b69a2e4
Mary-Afrika <marygahar@gmail.com>
2023-05-22 13:30:03 -0700

    fixed aray

--- memory.c
+++ memory.c
@@ -1,20 +1,20 @@
 #include "shell.h"
 
 /**
- * free_char_array - frees memory allocated for a string array
- * @arr: the string array to free
- * Return: void
- */
+* free_char_array - frees memory allocated for a string array
+* @array: the string array to free
+* Return: void
+*/
 void free_char_array(char **array)
 {
-    int i;
+int i;
 
-    if (array == NULL)
-        return;
+if (array == NULL)
+return;
 
-    for (i = 0; array[i] != NULL; i++)
-    {
-        free(array[i]);
-    }
-    free(array);
+for (i = 0; array[i] != NULL; i++)
+{
+free(array[i]);
+}
+free(array);
 }
