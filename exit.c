ff1ff389007a8911d0569aa0f20590718112cb3f
Mary-Afrika <marygahar@gmail.com>
2023-05-22 11:13:15 -0700

    added descrption

--- exit.c
+++ exit.c
@@ -1,20 +1,25 @@
 #include "shell.h"
 
+/**
+* my_exit - exit the program with a specified status
+* @args: command arguments
+*/
+
 void my_exit(char **args)
 {
-    int status = 0;
-    long int str_result;
-    static int error_num = 1;
+int status = 0;
+long int str_result;
+static int error_num = 1;
 
-    if (args[1])
-    {
-        str_result = custom_atoi(args[1]);
-        if (str_result == 0)
-        {
-            printf("sh: %d: exit: Illegal number: %s\n", error_num++, args[1]);
-            return;
-        }
-        status = str_result;
-    }
-    exit(status);
+if (args[1])
+{
+str_result = custom_atoi(args[1]);
+if (str_result == 0)
+{
+perror("sh: %d: exit: Illegal number: %s\n", error_num++, args[1]);
+return;
+}
+status = str_result;
+}
+exit(status);
 }
