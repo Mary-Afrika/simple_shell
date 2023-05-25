bbc1054be2598e4b8cfb1269921eb86756c76003
Mary-Afrika <marygahar@gmail.com>
2023-05-25 10:48:07 -0700

    shell

--- paths.c
+++ paths.c
@@ -1,92 +1,47 @@
 #include "shell.h"
 
-/**
-* get_path - takes path from promt and checks if path is valid. Returns
-* the valid path, else null
-* @path: the partial path of to be checked if valid
-* Return: return a pointer to the path string, else NULL
-*/
-char *get_path(char *path)
-{
-char *full_path = NULL;
-char **dirs;
-struct stat st;
-int i;
+char *get_path(char *command) {
+    char *path = getenv("PATH");
+	char *dir = NULL;
+	char *path_copy = NULL;
+	struct stat st;
+	char *valid_path = NULL;
+	char *full_path = NULL;
 
-dirs = get_directories();
+    if (path == NULL) {
+        perror("Unable to access the PATH environment variable.");
+        return (NULL);
+    }
 
-for (i = 0; dirs[i]; i++)
-{
-full_path = malloc(_strlen(path) + _strlen(dirs[i]) + 2);
+	if (stat(command, &st) == 0) /*check if a path on its own*/
+	{
+        return _strdup(command);
+    }
 
-if (!full_path)
-{
-perror("Memory Allocation Failed");
-return (NULL);
-}
-_strcat(full_path, dirs[i]);
-_strcat(full_path, "/");
-_strcat(full_path, path);
-_strcat(full_path, "\0");
+    path_copy = _strdup(path);
+    if (path_copy == NULL) {
+        perror("strdup: ");
+        return (NULL);
+    }
+    dir = strtok(path_copy, ":");
+    while (dir != NULL) {
+		full_path = malloc((_strlen(dir) + _strlen(command) + 2) * sizeof(char));
+		if (full_path == NULL) {
+			perror("malloc: ");
+			return (NULL);
+		}
+		strcat(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path, command);
 
-if (stat(full_path, &st) == 0)
-{
-free(dirs);
-return (full_path);
-}
-else
-{
-free(full_path);
-}
-}
-if (stat(path, &st) == 0)
-{
-free_char_array(dirs);
-return (_strdup(path));
-}
-return (NULL);
-}
-
-
-/**
-* get_directories - returns array of directories in PATH
-* Return: char**, a string array of the directories
-*/
-char **get_directories(void)
-{
-char *path = getenv("PATH");
-char **dirs;
-int i = 0;
-char *dir;
-char *path_copy;
-
-if (path == NULL)
-{
-perror("PATH not set\n");
-return (NULL);
-}
-dirs = malloc(1024 * sizeof(char *));
-if (!dirs)
-{
-	perror("Malloc fialed");
-	return (NULL);
-}
-path_copy = strdup(path);
-if (!path_copy)
-{
-perror("Memory Allocation Failed");
-free_char_array(dirs);
-return (NULL);
-perror("Memory Allocation Failed");
-return (NULL);
-}
-dir = strtok(path_copy, ":");
-while (dir != NULL && i < 1024)
-{
-dirs[i] = dir;
-i++;
-dir = strtok(NULL, ":");
-}
-dirs[i] = NULL;
-return (dirs);
+        if (stat(full_path, &st) == 0) {
+            valid_path = _strdup(full_path);
+			break;
+        }
+        dir = strtok(NULL, ":");
+		free(full_path);
+    }
+	free(full_path);
+	free(path_copy);
+    return (valid_path);
 }
