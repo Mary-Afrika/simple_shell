cd6e32c4e4f0e0a8a527a5a1ce22a4df8268d75a
kmishmael <kmishmael@gmail.com>
2023-05-27 00:51:17 +0300

    fix exit fn

--- exit.c
+++ exit.c
@@ -12,7 +12,6 @@ void my_exit(char *command, char **args, char **av, int *status)
 int error_no = 0;
 long int str_result;
 static int error_num = 1;
-char err_ch;
 
 (void)status;
 if (args[1])
@@ -20,13 +19,16 @@ if (args[1])
 str_result = custom_atoi(args[1]);
 if (str_result == 999999 || str_result < 0)
 {
-err_ch = (char)error_num;
-(void)err_ch;
 fprintf(stderr, "%s: %d: exit: Illegal number: %s\n", av[0],
 		error_num++, args[1]);
-return;
+free(args);
+free(command);
+exit(2);
 }
+else
+{
 error_no = str_result;
+}
 }
 else
 {
